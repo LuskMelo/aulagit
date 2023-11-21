179720b355a04428bc165872be18b82aeef79f47
LuskMelo <luskmelo@gmail.com>
2023-11-21 19:45:18 -0300

    Atualizei de novo

--- src/Proj2/Funcionario.java
+++ src/Proj2/Funcionario.java
@@ -3,26 +3,25 @@ package Proj2;
 import java.util.Scanner;
 
 public class Funcionario {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
 
-		 int n1, n2;
-		 double n3;
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
 
-		System.out.println("Digite o número do funcionário");
-		n1 = sc.nextInt();
-		System.out.println("Digite as horas trabalhadas pelo funcionario");
-		n2 = sc.nextInt();
-		System.out.println("Digite o valor que recebe por hora");
-		n3 = sc.nextDouble();
-		
+    int n1, n2;
+    double n3;
 
-		sc.close();
-		
-		double salario = (n2 * n3);
-		
-		System.out.println("Funcionário n° " + n1);
-		System.out.printf("salário = R$ %.2f", + salario);
-		
-	}
+    System.out.println("Digite o número do funcionário");
+    n1 = sc.nextInt();
+    System.out.println("Digite as horas trabalhadas pelo funcionario");
+    n2 = sc.nextInt();
+    System.out.println("Digite o valor que recebe por hora");
+    n3 = sc.nextDouble();
+
+    sc.close();
+
+    double salario = (n2 * n3);
+
+    System.out.println("Funcionário n° " + n1);
+    System.out.printf("salário = R$ %.2f", +salario);
+  }
 }
